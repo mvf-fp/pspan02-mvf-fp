659d3ec0fc541ca7a240a5af4038ee29f55b30ef
mvf-fp <m.vera@fpfempa.es>
2025-11-07 11:14:47 +0100

    AN02 Ej3

---
+++ ej03/main.go
@@ -0,0 +1,36 @@
+﻿package main
+
+import (
+    "flag"
+    "fmt"
+    "os"
+)
+
+func main() {
+    // Definir los flags
+    fileName := flag.String("f", "output.txt", "Nombre del fichero a crear")
+    content := flag.String("c", "Hello, Go!", "Contenido a escribir en el fichero")
+    repeat := flag.Int("n", 1, "Número de veces que se escribirá el contenido")
+
+    // Procesar los flags
+    flag.Parse()
+
+    // Crear o sobrescribir el fichero
+    file, err := os.Create(*fileName)
+    if err != nil {
+        fmt.Println("Error al crear el fichero:", err)
+        return
+    }
+    defer file.Close()
+
+    // Escribir el contenido n veces
+    for i := 0; i < *repeat; i++ {
+        _, err := file.WriteString(fmt.Sprintf("%s\n", *content))
+        if err != nil {
+            fmt.Println("Error al escribir en el fichero:", err)
+            return
+        }
+    }
+
+    fmt.Printf("Fichero '%s' generado correctamente con %d línea(s).\n", *fileName, *repeat)
+}
